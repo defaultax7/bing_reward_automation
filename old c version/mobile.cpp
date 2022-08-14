a4732744e3bb287abf9af1990f1a946679ebebdf
defaultax7 <49340293+defaultax7@users.noreply.github.com>
2022-08-14 15:18:10 +0800

    upload poor c++ version

---
+++ old c version/mobile.cpp
@@ -0,0 +1,53 @@
+#define WINVER 0x0500
+#include <windows.h>
+
+ INPUT ip;
+
+void key_press(char code){
+	ip.ki.wVk = code; 
+	ip.ki.dwFlags = 0; // 0 for key press
+	SendInput(1, &ip, sizeof(INPUT));
+
+	ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+	SendInput(1, &ip, sizeof(INPUT));
+	
+	Sleep(100);
+}
+
+int main()
+{
+    // This structure will be used to create the keyboard
+    // input event.
+
+	int count = 0;
+	char tab = 0x09;
+	char a = 0x41;
+	char enter = 0x0D;
+	
+	while(!(GetKeyState(enter) & 0x8000)){
+
+	}
+
+	Sleep(500);
+
+	while(count <= 30){
+
+		// Set up a generic keyboard event.
+		ip.type = INPUT_KEYBOARD;
+		ip.ki.wScan = 0; // hardware scan code for key
+		ip.ki.time = 0;
+		ip.ki.dwExtraInfo = 0;
+		
+		key_press(tab);
+		key_press(a);
+		key_press(tab);
+		key_press(enter);
+		
+		Sleep(500);
+		++count;
+	}
+
+
+    // Exit normally
+    return 0;
+}
---
+++ old c version/readme.txt
@@ -0,0 +1,1 @@
+it is not fully automated, to use it you need to disable javascript (in Chrome/Edge press F12 and press ctrl+shift+p and type javascript), then do the first search with enter. The script will start running
